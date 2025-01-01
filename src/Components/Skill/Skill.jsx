208863438f6196039e713595c539097d7f0c3dff
Sreeharsh k <sreeharshk92@gmail.com>
2025-01-01 10:56:12 +0530

    skills 3D images added

--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -3,20 +3,31 @@ import { BiSolidLeftArrow, BiSolidRightArrow } from "react-icons/bi";
 import './Skill.css'
 import html from "../images/html.png";
 import css from "../images/css.png";
-import js from "../images/js.png";
+import js from "../images/javascript.png";
+import tailwind from "../images/tailwind.png";
+import laravel from "../images/laravel.png";
+import php from "../images/php.png";
 import git from "../images/git.png";
 import github from "../images/github.png";
 import react from "../images/react.png";
-import { h1, main } from "framer-motion/client";
+import aws from "../images/aws.png";
+import mysql from "../images/mysql.png";
+import firebase from "../images/firebase.png";
+
 
 const skills = [
   { id: 1, imgpath: html, name:"HTML" },
   { id: 2, imgpath: css, name:"CSS" },
   { id: 3, imgpath: js, name:"JavaScript" },
-  { id: 4, imgpath: git, name:"Git" },
+  { id: 4, imgpath: tailwind, name:"Tailwind" },
   { id: 5, imgpath: react, name:"React" },
-  { id: 6, imgpath: github, name:"GitHub" },
-  { id: 7, imgpath: html, name:"HTML" },
+  { id: 6, imgpath: php, name:"PHP" },
+  { id: 7, imgpath: laravel, name:"Laravel" },
+  { id: 8, imgpath: mysql, name:"MySQL" },
+  { id: 9, imgpath: git, name:"Git" },
+  { id: 10, imgpath: github, name:"GitHub" },
+  { id: 11, imgpath: aws, name:"AWS" },
+  { id: 12, imgpath: firebase, name:"Firebase" },
 ];
 
 const Skill = () => {
@@ -61,11 +72,10 @@ const Skill = () => {
   return (
     <main className="  flex justify-center bg-customColor ">
 
-   
-    <section className=" md:w-[30rem] w-[28rem] lg:w-[40rem] py-5 ">
-      <div className="relative w-full  px-10">
+    <section className=" md:w-[30rem] w-[28rem] lg:w-[58rem] py-5">
+      <div className="relative w-full">
         <button
-          className="absolute left-0 top-[50%] z-10 p-5 text-white hover:text-yellowColor"
+          className="absolute left-0 top-[50%] z-10 text-white hover:text-yellowColor"
           style={{
             transform: "translateY(-50%)",
             cursor: canScrollLeft ? "pointer" : "not-allowed",
@@ -77,11 +87,11 @@ const Skill = () => {
           <BiSolidLeftArrow />
         </button>
 
-        <div ref={scrollRef} className="py-3 flex overflow-x-auto hide-scrollbar" style={{ scrollBehavior: "smooth" }}onScroll={checkScroll}>
+        <div ref={scrollRef} className="py-3  flex overflow-x-auto hide-scrollbar" style={{ scrollBehavior: "smooth" }}onScroll={checkScroll}>
           {skills.map((skill) => (
           
-            <div key={skill.id} className="flex justify-center flex-shrink-0 w-[25%]">
-              <img className="object-cover lg:h-[5rem] lg:w-[5rem] h-16 w-16" src={skill.imgpath} alt="skills-png" />
+            <div key={skill.id} className="flex justify-center flex-shrink-0 w-[25%]  ">
+              <img className="object-cover hover:-translate-y-2.5 transition duration-500 ease-in-out lg:h-[10rem] lg:w-[10rem] h-16 w-16" src={skill.imgpath} alt="skills-png" />
             </div>
           
           ))}
